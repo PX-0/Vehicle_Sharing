44acf4900c75a1c7e32cd56cc0a86f72a4362c04
black9807 <nic1200182@gmail.com>
2022-12-16 16:24:32 +0100

    entity

--- spring-vehicle_sharing/src/main/java/com/springvehicle_sharing/entities/Prenotazione.java
+++ spring-vehicle_sharing/src/main/java/com/springvehicle_sharing/entities/Prenotazione.java
@@ -3,6 +3,7 @@ package com.springvehicle_sharing.entities;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,7 +11,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "prenotazioni")
 public class Prenotazione {
 	
 	@Id
