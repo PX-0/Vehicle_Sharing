1dcb8072804cba764ed69e0b50b194b3413b46fe
rottenapple-tech <dam.cyb.act@gmail.com>
2022-12-16 11:52:10 +0100

    start

---
+++ spring-vehicle_sharing/src/main/java/com/springvehicle_sharing/SpringVehicleSharingApplication.java
@@ -0,0 +1,13 @@
+package com.springvehicle_sharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVehicleSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVehicleSharingApplication.class, args);
+	}
+
+}
---
+++ spring-vehicle_sharing/src/test/java/com/springvehicle_sharing/SpringVehicleSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springvehicle_sharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVehicleSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
