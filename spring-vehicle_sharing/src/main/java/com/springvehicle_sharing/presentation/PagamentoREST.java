066dc6610b37c6272908f546199d82bfd893639c
luisabizik <luisa.bizik@gmail.com>
2022-12-16 16:16:31 +0100

    Create PagamentoREST.java
    pagamento rest

---
+++ spring-vehicle_sharing/src/main/java/com/springvehicle_sharing/presentation/PagamentoREST.java
@@ -0,0 +1,41 @@
+package com.springvehicle_sharing.presentation;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springvehicle_sharing.dal.PagamentoDAO;
+import com.springvehicle_sharing.entities.Pagamento;
+
+@RestController
+@RequestMapping("api")
+public class PagamentoREST {
+	
+	@Autowired
+	private PagamentoDAO pagDao;
+	
+	@GetMapping("pagamento")
+	public List<Pagamento> findAll(){
+		return pagDao.findAll();
+	}
+	
+	@GetMapping("pagamento/{id}")
+	public Pagamento findById(@PathVariable("id") Integer id){
+		return pagDao.findById(id).get();
+	}
+	
+	@PostMapping("pagamento")
+	public Pagamento addPagamento(@RequestBody Pagamento p) {
+		return pagDao.save(p);
+	}
+	
+	
+
+	
+}
