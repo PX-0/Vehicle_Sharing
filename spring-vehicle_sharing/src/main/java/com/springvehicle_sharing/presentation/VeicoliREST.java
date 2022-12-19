d1a78ccf0eb0b5663b80200d2386a9c9dcdfb919
black9807 <nic1200182@gmail.com>
2022-12-19 20:11:52 +0100

    edit form

--- spring-vehicle_sharing/src/main/java/com/springvehicle_sharing/presentation/VeicoliREST.java
+++ spring-vehicle_sharing/src/main/java/com/springvehicle_sharing/presentation/VeicoliREST.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,5 +40,9 @@ public class VeicoliREST {
 		return dao.save(v);
 	}
 	
+	@DeleteMapping("veicoli/{id}")
+	public void deleteVeicolo(@PathVariable("id") String id) {
+		dao.deleteById(id);
+	}
 
 }
