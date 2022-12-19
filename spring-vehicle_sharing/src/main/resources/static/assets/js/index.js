893a6525ba6b6a33fce2cde80cf191ac8cbae132
Dario Mucci <112767790+PX-0@users.noreply.github.com>
2022-12-19 14:54:58 +0100

    ok

--- spring-vehicle_sharing/src/main/resources/static/assets/js/index.js
+++ spring-vehicle_sharing/src/main/resources/static/assets/js/index.js
@@ -25,9 +25,9 @@ const swiper = new Swiper('.swiper', {
     loop: true,
   
     // // If we need pagination
-    // pagination: {
-    //   el: '.swiper-pagination',
-    // },
+    pagination: {
+      el: '.swiper-pagination',
+    },
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
