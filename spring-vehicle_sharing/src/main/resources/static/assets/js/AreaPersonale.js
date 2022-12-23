5c361f9e9e87548fdb568cc2a6b81cc5306c0cf5
Dario Mucci <112767790+PX-0@users.noreply.github.com>
2022-12-23 20:09:04 +0100

    okok areaPersonale e card
    se qualcuno trova soluzioni migliori sarebbe top

--- spring-vehicle_sharing/src/main/resources/static/assets/js/AreaPersonale.js
+++ spring-vehicle_sharing/src/main/resources/static/assets/js/AreaPersonale.js
@@ -1,23 +1,6 @@
 const swiper = new Swiper('.swiper', {
-    slidesPerView: 1,
-    spaceBetween: 10,
-
-    // Responsive breakpoints
-    breakpoints: {
-
-      // when window width is >= 576px
-      576: {
-        slidesPerView: 2,
-        spaceBetween: 10
-      },
-
-      // when window width is >= 992px
-      992: {
-        slidesPerView: 3,
-        spaceBetween: 10
-      }
-
-    },
+  slidesPerView: 1,
+  spaceBetween: 10,
 
   // Optional parameters
   direction: 'horizontal',
@@ -40,12 +23,13 @@ const swiper = new Swiper('.swiper', {
 });
 
 const titleAreaPersonale = document.querySelector('#hello-utente');
+const messaggioNotBooking = document.querySelector('#msg-NotBooking');
+const sectionTitlePrenotazioni = document.querySelector('#prenotazioni-utente');
 const userData = document.querySelector('#user-session-data').textContent;
-const sectionPrenotazioni = document.querySelector('#prenotazioni-utente');
 
 const swiperBox = document.querySelector('#swiper-box');
 
-const urlVeicoli= "http://localhost:9014/api/prenotazioniByUtente/";
+const urlVeicoli = "http://localhost:9014/api/prenotazioniByUtente/";
 
 var infoUser = userData.split(',');
 
@@ -53,99 +37,106 @@ var infoUser = userData.split(',');
 
 /* ----------------------------- END CONSTs&VARs ---------------------------- */
 
-async function requestGetVeicoli(){
-    var response = await fetch(urlVeicoli+infoUser[2])
-    var data = await response.json();
-    if(data != undefined){
-      var h2 = document.createElement('h2');
-      h2.textContent = "LE TUE PRENOTAZIONI";
-      sectionPrenotazioni.appendChild(h2);
-      data.forEach(element=>
-          CreateCard(
-              element.veicolo.immagineVeicolo,
-              element.dataPrenotazione,
-              element.veicolo.posizioneAttuale)
-      
-      )
-        }
+async function requestGetVeicoli() {
+  var response = await fetch(urlVeicoli + infoUser[2])
+  var data = await response.json();
+  console.log(data);
+  if (data.length !=0) {
+    var h2 = document.createElement('h2');
+    h2.textContent = "LE TUE PRENOTAZIONI";
+    sectionTitlePrenotazioni.appendChild(h2);
+    data.forEach(element =>
+      CreateCard(
+        element.veicolo.immagineVeicolo,
+        element.dataPrenotazione,
+        element.veicolo.posizioneAttuale)
+
+    )
+  } else {
+    var msgError = document.createElement('h2');
+    msgError.textContent = "CI DISPIACE, NON ABBIAMO TROVATO PRENOTAZIONI A SUO NOME";
+    messaggioNotBooking.appendChild(msgError);
+  }
 }
 
 /* --------------------------------- END ASYNCS --------------------------------- */
 function getDataFormattata(data) {
 
-    var date = data.split("T");
+  var date = data.split("T");
 
-    const array = date[0].split('-');
+  const array = date[0].split('-');
 
-    return `${array[2]}/${array[1]}/${array[0]}`;
+  return `${array[2]}/${array[1]}/${array[0]}`;
 
 }
 
-function CreateCard(url,data,posizione){
+function CreateCard(url, data, posizione) {
 
-    //creo swiper-slide
-    var swiperSlide = document.createElement('div');
-    swiperSlide.classList.add('swiper-slide');
-  
-    //inizio a creare la card
-    var card = document.createElement('div');
-    card.classList.add('card');
+  //creo swiper-slide
+  var swiperSlide = document.createElement('div');
+  swiperSlide.classList.add('swiper-slide');
 
-    //immagine
-    var img = document.createElement('img');
-    img.classList.add('card-img-top');
-    if(url.startsWith('http')){
-        img.setAttribute('src',url);
-        
-      }else if(url==null){
-        
-        img.setAttribute('src',"../uploads/elementor-placeholder-image.jpg");
-        
-      }else{
-        img.setAttribute('src',"../assets/uploads/"+url);
-      }
+  //inizio a creare la card
+  var card = document.createElement('div');
+  card.classList.add('card');
 
-    card.appendChild(img)
-    
-    var cardBody = document.createElement('div');
-    cardBody.classList.add('card-body');
+  //immagine
+  var div = document.createElement('div');
+  div.classList.add('img-fx');
+  var img = document.createElement('img');
+  img.classList.add('card-img-top');
+  if (url.startsWith('http')) {
+    img.setAttribute('src', url);
 
-    var cardText = document.createElement('div');
-    cardText.classList.add('card-text','text-center');
+  } else if (url == null) {
 
-    var dataPrenotazione = document.createElement('p');
-    dataPrenotazione.textContent= "Prenatata il: "+ getDataFormattata(data);
-    
-    var luogo = document.createElement('p');
-    luogo.textContent= "Si trova in: "+posizione;
+    img.setAttribute('src', "../uploads/elementor-placeholder-image.jpg");
 
-    cardText.appendChild(dataPrenotazione);
-    cardText.appendChild(luogo);
-
-    cardBody.appendChild(cardText);
-    
-    card.appendChild(cardBody);
-
-    swiperSlide.appendChild(card);
-  
-    swiperBox.appendChild(swiperSlide);
+  } else {
+    img.setAttribute('src', "../assets/uploads/" + url);
   }
+  div.appendChild(img)
+  card.appendChild(div)
+
+  var cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+
+  var cardText = document.createElement('div');
+  cardText.classList.add('card-text', 'text-center');
+
+  var dataPrenotazione = document.createElement('p');
+  dataPrenotazione.textContent = "Prenatata per il: " + getDataFormattata(data);
+
+  var luogo = document.createElement('p');
+  luogo.textContent = "Si trova in: " + posizione;
+
+  cardText.appendChild(dataPrenotazione);
+  cardText.appendChild(luogo);
+
+  cardBody.appendChild(cardText);
+
+  card.appendChild(cardBody);
+
+  swiperSlide.appendChild(card);
+
+  swiperBox.appendChild(swiperSlide);
+}
 
 
 
 
 
 function createWelcome() {
-    var h1 = document.createElement('h1');
-    var welcome;
-    if (infoUser[0].endsWith("a")) {
-        welcome = "BENVENUTA"
-    }else{
-        welcome = "BENVENUTO"
-    }
-    h1.textContent = welcome+" "+infoUser[0]+" "+ infoUser[1]
+  var h1 = document.createElement('h1');
+  var welcome;
+  if (infoUser[0].endsWith("a")) {
+    welcome = "BENVENUTA"
+  } else {
+    welcome = "BENVENUTO"
+  }
+  h1.textContent = welcome + " " + infoUser[0] + " " + infoUser[1]
 
-    titleAreaPersonale.appendChild(h1);
+  titleAreaPersonale.appendChild(h1);
 }
 /* ------------------------------ END FUNCTIONs ----------------------------- */
 
